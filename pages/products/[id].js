3ff7319449e91006ba0d34a9c83584bff7e81710
sakinal niraj <sakinalniraj11@gmail.com>
2025-01-22 08:19:54 +0530

    abcd

---
+++ pages/products/[id].js
@@ -0,0 +1,32 @@
+export async function getServerSideProps(context) {
+    const { id } = context.params;
+
+    // Fetch the product data from the API
+    const res = await fetch(`https://dummyjson.com/products/${id}`);
+
+    const data = await res.json();
+
+    return {
+        props: {
+            product: data,
+        },
+    };
+}
+
+function Product({ product }) {
+    return (
+        <div>
+            {product ? (
+                <div>
+                    <h1>{product.title}</h1>
+                    <p>{product.description}</p>
+                    <p>Price: ${product.price}</p>
+                </div>
+            ) : (
+                <p>No product found</p>
+            )}
+        </div>
+    );
+}
+
+export default Product;
